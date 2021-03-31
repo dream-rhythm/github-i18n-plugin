470a60962bb242415102270a1d9266d34aa6ce02
dream-rhythm <mailtonick1997@gmail.com>
2021-03-31 15:54:37 +0800

    Fix chinese

--- github-i18n.user.js
+++ github-i18n.user.js
@@ -1,21 +1,23 @@
 // ==UserScript==
 // @name                GitHub Internationalization
+// @name:zh-TW          GitHub中文化附加元件
 // @name:zh-CN          GitHub汉化插件
 // @name:ja             GitHub日本語
-// @namespace           https://github.com/k1995/github-i18n-plugin/
+// @namespace           https://github.com/dream-rhythm/github-i18n-plugin/
 // @version             0.10
 // @description         Translate GitHub.com
-// @description:zh      GitHub汉化插件，包含人机翻译
+// @description:zh-TW   GitHub漢化插件，包含人機翻譯
+// @description:zh      GitHub漢化插件，包含人機翻譯
 // @description:zh-CN   GitHub汉化插件，包含人机翻译
 // @description:ja      GitHub日本語プラグイン
-// @author              k1995
+// @author              dream-rhythm
 // @match               https://github.com/*
 // @match               https://gist.github.com/*
 // @grant               GM_xmlhttpRequest
 // @grant               GM_getResourceText
 // @resource            zh-CN  https://raw.githubusercontent.com/dream-rhythm/github-i18n-plugin/master/locales/zh-CN.json
 // @resource            ja  https://raw.githubusercontent.com/dream-rhythm/github-i18n-plugin/master/locales/ja.json
-// @resource            ja https://raw.githubusercontent.com/dream-rhythm/github-i18n-plugin/master/locales/zh-TW.json
+// @resource            zh-TW https://raw.githubusercontent.com/dream-rhythm/github-i18n-plugin/master/locales/zh-TW.json
 // @require             https://cdn.bootcdn.net/ajax/libs/timeago.js/4.0.2/timeago.full.min.js
 // @require             https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
 // ==/UserScript==
@@ -23,7 +25,7 @@
 (function() {
   'use strict';
 
-  const SUPPORT_LANG = ["zh-CN", "ja"];
+  const SUPPORT_LANG = ["zh-TW", "zh-CN", "ja"];
   const lang = (navigator.language || navigator.userLanguage);
   const locales = getLocales(lang)
 
@@ -33,9 +35,6 @@
   watchUpdate();
 
   function getLocales(lang) {
-    // if(lang.startsWith("zh")) { // zh zh-TW --> zh-CN
-    //   lang = "zh-CN";
-    // }
     if(SUPPORT_LANG.includes(lang)) {
       return JSON.parse(GM_getResourceText(lang));
     }
@@ -127,7 +126,7 @@
   // translate "about"
   function translateDesc() {
     $(".repository-content .f4").append("<br/>");
-    $(".repository-content .f4").append("<a id='translate-me' href='#' style='color:rgb(27, 149, 224);font-size: small'>翻译</a>");
+    $(".repository-content .f4").append("<a id='translate-me' href='#' style='color:rgb(27, 149, 224);font-size: small'>翻譯</a>");
     $("#translate-me").click(function() {
       // get description text
       const desc = $(".repository-content .f4")
@@ -154,7 +153,7 @@
             $(".repository-content .f4").append("<br/>");
             $(".repository-content .f4").append(text);
           } else {
-            alert("翻译失败");
+            alert("翻譯失敗");
           }
         }
       });
